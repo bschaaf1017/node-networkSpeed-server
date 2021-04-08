237ad6ff2857e23fb639e6d66819fbaf8b05fbad
bschaaf1017 <brian.schaaf@epicvideo.tech>
2021-04-08 16:56:10 -0400

    added endpoint fopr dowloading jpg

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const parser = require('body-parser');
+const path = require('path')
 
 const PORT = 3000;
 const app = express();
+
+const dir = path.join(__dirname, '..', 'assets', 'space.jpg') 
 
 app.use(parser({limit: '50mb'}));
 app.use(express.json());
@@ -10,6 +13,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
   console.log("Get Request")
   res.end();
+})
+
+app.get('/dlSpeed', (req, res) => {
+  console.log("DLSPEED")
+  res.sendFile(dir)
 })
 
 app.post("/speed",(req, res) => {
