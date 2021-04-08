9f3dce54acb8fc5a2be04a651247f69f75d80059
maximossalzman <maximos.salzman@epicvideo.tech>
2021-04-08 16:32:19 -0400

    updates index.js

--- server/index.js
+++ server/index.js
@@ -7,17 +7,19 @@ const app = express();
 app.use(parser({limit: '50mb'}));
 app.use(express.json());
 
-app.get((req, res) => {
-  console.log('hello world'); {
-    if (err) {
-      res.writeHead(400);
-      res.end();
-    } else {
-      res.send({resp: "it worked"});
-    }
-  }
+app.get("/", (req, res) => {
+  console.log("Get Request")
+  res.end();
 })
 
+app.post("/speed",(req, res) => {
+  console.log('hello world'); 
+
+  res.send({resp: "it worked"});
+  
+  }
+)
+
 app.listen(PORT, () => {
-  console.log(`Server listening at localhost:${PORT}!`);
+  console.log(`Server listening at ${PORT}!`);
 });
